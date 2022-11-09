1977eb098cc1560ce7cd255c330e9978fbd297e9
Jay <jaysebagh@gmail.com>
2022-11-09 14:07:48 -0800

    Input field now checks whether the current typed letters match the current word in the paragraph.
    Incorrect letters are identified, currently as a console.log().
    
    Input resets if the word is completed and spacebar is pressed.
    
    Properly handles index position of letters in paragraph if backspace is pressed and allows user to reattempt to type the word properly.

--- src/components/text.js
+++ src/components/text.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import './sass/text.sass'
 const randomWords = require('random-words');
 
@@ -15,21 +15,51 @@ const Text = () => {
         handleParagraphSize(size);
     };
 
+    let letters = paragraph.split("");
+    console.log(letters)
+
+    // used to check input value while in the handleKeyDown function
+    // https://bobbyhadz.com/blog/react-get-input-value
+    const inputRef = useRef(null);
+
+    let i = 0;
+    const handleChange = (e) => {
+        // grab the last letter in the input
+        const lastLetter = e.target.value.slice(-1)
+        // check if the current input letter matches the current paragraph letter
+        if(lastLetter === letters[i]){
+            console.log(letters[i])
+            // reset input to blank if the current paragraph letter is a space
+            if(lastLetter === " " && letters[i] === " ") e.target.value = ""
+            i++
+        } else {
+            console.log("mistake")
+            i++
+        }
+    }
+
+    const handleKeyDown = (e) => { if(e.keyCode === 8 && inputRef.current.value !== "") i -= 2 }
+
     console.log(paragraph)
     return(
         <div className = "textControl">
             <div className = "sizeButtons">
-                <button className = "size" onClick = {() => handleParagraphSize(10)}>10</button>
-                <button className = "size" onClick = {() => handleParagraphSize(25)}>25</button>
-                <button className = "size" onClick = {() => handleParagraphSize(50)}>50</button>
-                <button className = "size" onClick = {() => handleParagraphSize(75)}>75</button>
-                <button className = "size" onClick = {() => handleParagraphSize(100)}>100</button>
+                <button className="size" onClick={() => handleParagraphSize(10)}>10</button>
+                <button className="size" onClick={() => handleParagraphSize(25)}>25</button>
+                <button className="size" onClick={() => handleParagraphSize(50)}>50</button>
+                <button className="size" onClick={() => handleParagraphSize(75)}>75</button>
+                <button className="size" onClick={() => handleParagraphSize(100)}>100</button>
             </div>
             <div className = "paragraph">
                 {paragraph}
             </div>
             <div>
-                <input className = "typeBox"/><button className = "redo" onClick = {() => handleRedo(size)}>REDO</button>
+                <input
+                    ref={inputRef}
+                    className="typeBox"
+                    onChange={handleChange}
+                    onKeyDown={handleKeyDown}
+                /><button className="redo" onClick = {() => handleRedo(size)}>REDO</button>
             </div>
         </div>
     )
